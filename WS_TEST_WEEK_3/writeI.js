d90511763cec9cdf467de3b3eb9540d4db241f8b
AudreyCoulombe <55247616+AudreyCoulombe@users.noreply.github.com>
2022-09-20 20:25:56 -0400

    write in file with append

--- WS_TEST_WEEK_3/writeI.js
+++ WS_TEST_WEEK_3/writeI.js
@@ -1,6 +1,17 @@
-// Run this file with terminal and it will create a new file in myFiles folfer
+// Run this file (note: writeI-->capital i, not an l) with terminal and it will create a new file in myFiles folfer
 const fs = require('fs'); // fs is installed in node core module (not folder but on computer)
 const fileContainer = './myFiles/fileA.txt';
 let data = "this is my test string written at "+(new Date)+"to "+fileContainer;
 fs.writeFileSync(fileContainer, data); //fs=file system; writefilesync=inbuilt
 
+// will over write first
+let dataTwo= "this is my second test string written at "+(new Date)+"to "+fileContainer;
+fs.writeFileSync(fileContainer, dataTwo);
+
+// will over write first and second
+let dataThird = "this is my third test string written at "+(new Date)+"to "+fileContainer+"\n"+"more text on second line";
+fs.writeFileSync(fileContainer, dataThird);
+
+// append doesnt create a file if it doesn<t already exist, but write file sync yes
+let textToAppend = "\n" + (new Date) + " Text copied and appended  to " + fileContainer;
+fs.appendFileSync(fileContainer, textToAppend);
