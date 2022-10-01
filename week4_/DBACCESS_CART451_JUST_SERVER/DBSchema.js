3b64810385321cdb7e9d61a5db7df22f71c366b3
AudreyCoulombe <55247616+AudreyCoulombe@users.noreply.github.com>
2022-10-01 14:45:23 -0400

    explorations with queries

--- week4_/DBACCESS_CART451_JUST_SERVER/DBSchema.js
+++ week4_/DBACCESS_CART451_JUST_SERVER/DBSchema.js
@@ -1,20 +1,36 @@
 const mongoose = require('mongoose');
 const fitBitSchema = new mongoose.Schema({
-    Id:String,
-    ActivityDate:String,
-    TotalSteps:String,
-    TotalDistance:String,
-    TrackerDistance:String,
-    LoggedActivitiesDistance:String,
-    VeryActiveDistance:String,
-    ModeratelyActiveDistance:String,
-    LightActiveDistance:String,
-    SedentaryActiveDistance:String,
-    VeryActiveMinutes:String,
-    FairlyActiveMinutes:String,
-    LightlyActiveMinutes:String,
-    SedentaryMinutes:String,
-    Calories:String
+    // Id:String,
+    // ActivityDate:String,
+    // TotalSteps:String,
+    // TotalDistance:String,
+    // TrackerDistance:String,
+    // LoggedActivitiesDistance:String,
+    // VeryActiveDistance:String,
+    // ModeratelyActiveDistance:String,
+    // LightActiveDistance:String,
+    // SedentaryActiveDistance:String,
+    // VeryActiveMinutes:String,
+    // FairlyActiveMinutes:String,
+    // LightlyActiveMinutes:String,
+    // SedentaryMinutes:String,
+    // Calories:String
+
+    Id:Number,
+    ActivityDate:Number,
+    TotalSteps:Number,
+    TotalDistance:Number,
+    TrackerDistance:Number,
+    LoggedActivitiesDistance:Number,
+    VeryActiveDistance:Number,
+    ModeratelyActiveDistance:Number,
+    LightActiveDistance:Number,
+    SedentaryActiveDistance:Number,
+    VeryActiveMinutes:Number,
+    FairlyActiveMinutes:Number,
+    LightlyActiveMinutes:Number,
+    SedentaryMinutes:Number,
+    Calories:Number
 });
 
 //const fitBitDataModel = mongoose.model("Fitbit_collection", fitBitSchema, fitbit_collection);
--- week4_/DBACCESS_CART451_JUST_SERVER/server.js
+++ week4_/DBACCESS_CART451_JUST_SERVER/server.js
@@ -11,7 +11,7 @@ const mongoose = require("mongoose");
 let bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-app.use('/varsToMongo',handleGetVars);
+app.use('/varsToMongo', handleGetVars);
 
 //get url from .env file
 const url = process.env.MONGODB_URI;
@@ -20,12 +20,23 @@ console.log(url);
 //Access data model (DBSchema.js)
 const fitBitModel = require("./DBSchema.js");
 
+
+
 //connect to database
 mongoose.connect(url);
 let db = mongoose.connection;
-db.once("open", async function(){
+db.once("open", async function () {
   console.log("connecting to DB");
-  fitBitModel.find({TotalSteps:"13162"}).then((result)=>{
+
+  // To convert/update each field in the collection to be from a string to an integer or decimal (float/double). (NOTE: only because my values in my db are written as a string and I are labeled as Numbers in DBSchema.js)
+  let test = await fitBitModel.updateMany({}, [{ $set: { "TrackerDistance": { $toDouble: "$TrackerDistance" } } }], { multi: true });
+  fitBitModel.find({ TrackerDistance: 9.80000019073486 }).then((result) => { // Find all documents with value 13162 for the key "TotalSteps"
+
+  //fitBitModel.findOne({TotalSteps:"13162"}).then((result)=>{ // Find only one document
+  //fitBitModel.find({TotalSteps:"15355", Calories:"2013"}).then((result)=>{ // Find document with value "15355" for the key "TotalSteps" and value "2013" for "Calories"
+  //fitBitModel.find({Calories:{$gte 1900}}).then((result)=>{ // Find document with value "15355" for the key "TotalSteps" and value "2013" for "Calories"
+  //fitBitModel.find({TotalSteps:"13162"}).then((result)=>{ // Find all documents with value 13162 for the key "TotalSteps"
+  //fitBitModel.findOne({TotalSteps:"13162"}).then((result)=>{
     console.log(result);
   })
 });
@@ -48,16 +59,16 @@ function clientRoute(req, res, next) {
 }
 
 /// use this VERB for getting posted data... 9
-app.post('/postForm',handlePost);
- 
+app.post('/postForm', handlePost);
+
 // the callback= log the results
-function handlePost(request,response){
+function handlePost(request, response) {
   console.log(request.body);
   response.send("SUCCESS POST");
 }
 
 //EXAMPLE of  user making a query ... 10
-async function  handleGetVars  (request,response,next){
+async function handleGetVars(request, response, next) {
   console.log(request.url);
   console.log(request.query.paramOne);
   response.send("SUCCESS GET");
